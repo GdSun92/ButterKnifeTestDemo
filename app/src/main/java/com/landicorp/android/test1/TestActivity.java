46f07ec8ea7112c18d6ec9a50dc0f91c40d610ae
GdSun <sgd1104@163.com>
2017-08-10 10:48:43 +0800

    第一次用Android Studio上传到gitHub

---
+++ app/src/main/java/com/landicorp/android/test1/TestActivity.java
@@ -0,0 +1,16 @@
+package com.landicorp.android.test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by GdSun on 2017/8/10.
+ */
+
+public class TestActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
