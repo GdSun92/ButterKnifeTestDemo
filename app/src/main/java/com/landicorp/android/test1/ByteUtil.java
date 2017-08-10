19f9fa676ba2bd54d8d592f35dd300b8e5481e1d
GdSun <sgd1104@163.com>
2017-08-10 11:23:46 +0800

    Update ByteUtil.java
    增加ByteUtil方法

--- app/src/main/java/com/landicorp/android/test1/ByteUtil.java
+++ app/src/main/java/com/landicorp/android/test1/ByteUtil.java
@@ -5,4 +5,13 @@ package com.landicorp.android.test1;
  */
 
 public class ByteUtil {
+  	public static String byte2Hex(byte b) {
+		StringBuilder buffer = new StringBuilder();
+			String hex = Integer.toHexString(b & 0xff);
+			if (hex.length() == 1) {
+				buffer.append('0');
+			}
+			buffer.append(hex);
+		return buffer.toString().toUpperCase();
+	}
 }
