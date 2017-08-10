69605885bd4d3960aad038d6b0b106b5bd3a7b97
GdSun <sgd1104@163.com>
2017-08-10 11:27:05 +0800

    Update ByteUtil.java

--- app/src/main/java/com/landicorp/android/test1/ByteUtil.java
+++ app/src/main/java/com/landicorp/android/test1/ByteUtil.java
@@ -14,4 +14,17 @@ public class ByteUtil {
 			buffer.append(hex);
 		return buffer.toString().toUpperCase();
 	}
+	
+		public static String bcd2Ascii(final byte[] bcd) {
+		if (bcd == null)
+			return "";
+    	StringBuilder sb = new StringBuilder(bcd.length << 1);
+    	for (byte ch : bcd) {
+    		byte half = (byte) (ch >> 4);
+    		sb.append((char)(half + ((half > 9) ? ('A' - 10) : '0')));
+    		half = (byte) (ch & 0x0f);
+    		sb.append((char)(half + ((half > 9) ? ('A' - 10) : '0')));
+    	}
+    	return sb.toString();
+    }
 }
