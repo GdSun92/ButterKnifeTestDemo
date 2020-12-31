f052017f01f078bec4833962701fbf3402a44984
GdSun <sgd1104@163.com>
2020-12-31 10:44:43 +0800

    Update MainActivity.java

--- app/src/main/java/com/landicorp/android/test/MainActivity.java
+++ app/src/main/java/com/landicorp/android/test/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends BaseActivity {
                 break;
             case R.id.button2:
                 ButterKnife.apply(button, ENABLE, true);
-                toast("this is a button2");
+                toast("this is a button2222");
                 break;
             default:
                 break;
